c5706e48912a0003086248bfba508bf753ee5322
parya <antoine020999@gmail.com>
2023-04-05 13:21:16 +0200

    commit 5

--- index.php
+++ index.php
@@ -1,41 +1,13 @@
 <?php
 include 'includes/var.php';
 include 'includes/func.php';
+include 'includes/header.php';
+include 'list.php';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tableau</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <table>
-        <tr>
-            <th>Article</th>
-            <th>prix</th>
-            <th>prix ht</th>
-            <th>description</th>
-
-        </tr>
-        <?php
-        foreach ($bonnets as $key => $bonnet) {
-            displayBonnets($bonnet)
-                ?>
-
-            <?php
-        } ?>
-
-
-
-    </table>
-
-</body>
-
-</html>
+    
+    <?php
+    include 'includes/footer.php';
+    ?>
 
---
+++ list.php
@@ -0,0 +1,23 @@
+
+
+
+
+<table>
+        <tr>
+            <th>Article</th>
+            <th>prix</th>
+            <th>prix ht</th>
+            <th>description</th>
+
+        </tr>
+        <?php
+        foreach ($bonnets as $key => $bonnet) {
+            displayBonnets($bonnet)
+                ?>
+
+            <?php
+        } ?>
+
+
+
+    </table>
