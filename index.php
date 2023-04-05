fd2da538d35e1cb6a6a04be90859905b9798171a
parya <antoine020999@gmail.com>
2023-04-05 18:11:13 +0200

    commit 8

--- index.php
+++ index.php
@@ -4,15 +4,16 @@ include 'includes/func.php';
 include 'includes/header.php';
 
 ?>
-
+    <div class="d-flex">
 <?php
+
 foreach ($bonnets as $key => $bonnet) {
     if ($key < 3) {
         rebond($bonnet);
-        
     }
 } ?>
-
+</div>
+<a class="button d-flex justify-content-center " href="list.php">Voir tout les produits</a>
 <?php
 include 'includes/footer.php';
 ?>
