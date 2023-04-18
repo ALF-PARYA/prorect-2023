a5a32da4a1bcc30c822b3ccc7ceb3f962f2fc137
parya <antoine020999@gmail.com>
2023-04-18 11:40:06 +0200

    commit 1 J4

--- pages/cart.php
+++ pages/cart.php
@@ -11,29 +11,33 @@ if (isset($_GET['id'])) {
 }
 
 var_dump($_SESSION['cart']);
-
-
-
-
-foreach ($_SESSION['cart'] as $id) {
-    ?>
-
-    <table class="table">
-        <tr>
-            <th>id</th>
-            <th>nom</th>
-            <th>prix</th>
-            <th>quantité</th>
-        </tr>
-        <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-
-        </tr>
-
-    </table>
-    <?php
-}
 ?>
+
+<table class="table">
+    <tr>
+        <th>id</th>
+        <th>nom</th>
+        <th>prix unitaire</th>
+        <th>quantité</th>
+        <th>prix total</th>
+    </tr>
+
+    <?php
+    var_dump($bonnets);
+    foreach ($_SESSION['cart'] as $id => $quantity) {
+        $bonnet = $bonnets[$id];
+        ?>
+
+        <tr>
+            <td><?php echo $id ?></td>
+            <td><?php echo $bonnet[0] ?></td>
+            <td><?php echo $bonnet[1] ?></td>
+            <td><?php echo $quantity ?> +</td>
+            <td><?php echo $quantity * $bonnet[1] ?>€</td>
+
+        </tr>
+
+        <?php
+    }
+    ?>
+</table>
--- pages/list.php
+++ pages/list.php
@@ -1,14 +1,7 @@
-<?php
-
-?>
-
-    <div class="d-flex justify-content-center">
-        <?php
-        foreach ($bonnets as $key => $bonnet) {
-            rebond($bonnet,$key);
-        } ?>
-        
-</div>
-
+<div class="d-flex justify-content-center">
     <?php
-?>
+    foreach ($bonnets as $key => $bonnet) {
+        rebond($bonnet, $key);
+    } ?>
+
+</div>
